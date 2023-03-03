834e7e295df2528de79a2a27853c145afb0fa0ee
Stella Quizas <sammko@gmail.com>
2023-03-04 03:52:46 +0800

    Enhance the user experience
    - always scroll to the current question/reply
    
    - flexible textarea
    
    - added style for code snippets
    
    - added scrollbar
    
    -

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import icon from './icon.png';
 import pig from './pig.png';
 import './App.css';
 import './normal.css';
-import { useState } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import React from 'react';
 import SyntaxHighlighter from 'react-syntax-highlighter';
 import { dracula } from 'react-syntax-highlighter/dist/esm/styles/hljs';
@@ -11,23 +11,80 @@ function App() {
   const [input, setInput] = useState('');
   const [chatLog, setChatLog] = useState([]);
 
+  const containerRef = useRef(null);
+
+  useEffect(() => {
+    const container = containerRef.current;
+    const observer = new MutationObserver((mutations) => {
+      mutations.forEach((mutation) => {
+        if (mutation.type === 'childList') {
+          // A child node has been added or removed
+          container.scrollTop = container.scrollHeight - container.clientHeight;
+        }
+      });
+    });
+    observer.observe(container, { childList: true });
+
+    // Cleanup function to disconnect the observer when the component unmounts
+    return () => observer.disconnect();
+  }, []);
+
   async function handleSubmit(e) {
     e.preventDefault();
-    console.log('submit: ' + input);
-    setChatLog((chatLog) => [...chatLog, { user: 'me', message: input }]);
+    // console.log('submit: ' + input);
+    setChatLog((chatLog) => [
+      ...chatLog,
+      { user: 'me', message: input.trim() },
+    ]);
     setInput('');
+    resetTextAreaChange(e);
     const response = await fetch('http://localhost:3001/', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify({
-        message: input,
+        message: input.trim(),
       }),
     });
     const data = await response.json();
 
     setChatLog((chatLog) => [...chatLog, { user: 'gpt', message: data.data }]);
+  }
+
+  // const scrollTop = () => {
+  //   let chatLog = document.querySelector('.chat-log');
+  //   //scroll to bottom
+  //   chatLog.scrollTop = chatLog.scrollHeight + chatLog.clientHeight;
+  //   // chatLog.scrollIntoView(false);
+  // };
+
+  const handleKeyDown = (e) => {
+    if (e.key === 'Enter' && !e.shiftKey) {
+      handleSubmit(e);
+    } else {
+      return;
+    }
+  };
+
+  const resetTextAreaChange = (e) => {
+    let textarea = document.querySelector('textarea');
+    // console.log('textarea' + textarea.value);
+    textarea.style.height = '34px';
+  };
+
+  const handleTextAreaChange = (e) => {
+    let textarea = document.querySelector('textarea');
+    // console.log('textarea' + textarea.value);
+    textarea.style.height = calcHeight(textarea.value) + 'px';
+  };
+
+  // // Dealing with Textarea Height
+  function calcHeight(value) {
+    let numberOfLineBreaks = (value.match(/\n/g) || []).length;
+    // min-height + lines x line-height + padding + border
+    let newHeight = 20 + numberOfLineBreaks * 20 + 12 + 2;
+    return newHeight;
   }
 
   return (
@@ -38,27 +95,23 @@ function App() {
         </div>
       </aside>
       <section className="chatbox">
-        <div className="chat-log">
+        <div className="chat-log" ref={containerRef}>
           {chatLog.map((message, index) => (
             <ChatMessage key={index} message={message} />
           ))}
         </div>
         <div className="query-input">
-          <form id="ask" onSubmit={handleSubmit}>
-            {/* <textarea
-              form="ask"
+          <form>
+            <textarea
               className="query-input-textarea"
-              rows="10"
+              rows="2"
               value={input}
-              onChange={(e) => setInput(e.target.value)}
+              onChange={(e) => {
+                setInput(e.target.value);
+                handleTextAreaChange(e);
+              }}
+              onKeyDown={handleKeyDown}
             ></textarea>
-            <input type="submit"></input> */}
-            <input
-              className="query-input-textarea"
-              rows="10"
-              value={input}
-              onChange={(e) => setInput(e.target.value)}
-            ></input>
           </form>
         </div>
       </section>
